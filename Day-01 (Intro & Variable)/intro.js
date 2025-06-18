3d5490bc8103c1679e1542ff833f243eddcd31de
GleanCoder <adityakiranacharya0@gmail.com>
2025-06-18 13:19:23 +0530

    Day 01 updated

---
+++ Day-01 (Intro & Variable)/intro.js
@@ -0,0 +1,28 @@
+// JS is just a programming language for web applications.
+
+// Internal Linking:write the js code inside the html file
+// write the js code in separate .js file
+console.log("Hello World!");
+
+//VARIABLES IN JS: var,let and const:
+
+// DataType varName=value; Strongly Typed Lang
+
+// but js is dynamic typed lang (loosly typed lang)
+
+var age = 22; // Global scope
+age = "Aditya";
+
+//Naming Conv.
+
+//1. firstname
+//2. first_name
+//3. FirstName
+//4. firstName
+
+let name = "Aditya"; // Local scoped, block scoped or function scoped
+
+const countryName = "India"; // Can't change once its value is assigned, local scoped.
+
+// Error: TypeError: Assignment to constant variable.
+// we can't keep undefined the const var
